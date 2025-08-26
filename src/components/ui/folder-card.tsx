76f9f3ede1f5fbe548ec8a99e004806321497d40
Teja Akella <takella6315@gmail.com>
2025-08-26 19:57:26 -0400

    deployment fixes

--- src/components/ui/folder-card.tsx
+++ src/components/ui/folder-card.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import { Card } from './card'
-import { Button } from './button'
+import { Card } from './card.tsx'
+import { Button } from './button.tsx'
 import { 
   FolderIcon, 
   FolderOpenIcon,
